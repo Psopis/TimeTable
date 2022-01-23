75a8d4935410cbe5a84af144e0ed77af2a22cc1a
Dima <ytude7042@gmail.com>
2022-01-23 17:22:57 +0700

    xexe

--- app/src/main/java/com/example/timetable/map/BusStopsRecycleAdapter.kt
+++ app/src/main/java/com/example/timetable/map/BusStopsRecycleAdapter.kt
@@ -3,7 +3,9 @@ package com.example.timetable.map
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.timetable.R
 import com.example.timetable.data.BusStopWithTime
@@ -14,7 +16,7 @@ class BusStopsRecycleAdapter(var current: Int, var dataSet: List<BusStopWithTime
 {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
     {
-        val View = LayoutInflater.from(parent.context).inflate(R.layout.busstop_item, parent, false)
+        val View = LayoutInflater.from(parent.context).inflate(R.layout.item_map, parent, false)
         return ViewHolder(View)
     }
 
@@ -28,17 +30,15 @@ class BusStopsRecycleAdapter(var current: Int, var dataSet: List<BusStopWithTime
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
         var context = itemView.context
-        var name_text = itemView.findViewById<TextView>(R.id.busStopName_busstop_item)
-        var time_text = itemView.findViewById<TextView>(R.id.time_busstop_item)
-
+        var name_text = itemView.findViewById<TextView>(R.id.textViewOst)
+        var time_text = itemView.findViewById<TextView>(R.id.textViewTime)
         fun onBind(busStopWithTime: BusStopWithTime)
         {
             if (dataSet[current] == busStopWithTime)
             {
-                itemView.setBackgroundColor( context.resources.getColor(R.color.light_blue) )
-//                itemView.findViewById<TextView>(R.id.textViewOst).setTextColor(context.resources.getColor(R.color.black))
-//                itemView.findViewById<TextView>(R.id.textViewTime).setTextColor(context.resources.getColor(R.color.grey_new))
-//                itemView.findViewById<ImageView>(R.id.imageView4).setImageDrawable(ContextCompat.getDrawable(context, R.drawable.baseline_lens_24))
+                itemView.findViewById<LinearLayout>(R.id.LinearLayout).setBackgroundColor( context.resources.getColor(
+                    R.color.purple_700
+                ) )
             }
 
 
